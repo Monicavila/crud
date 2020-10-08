d6f21b9fa9a2c4be49b1f0d59538affce3683d75
Monicavila <di.monicavila@gmail.com>
2020-10-07 22:13:44 -0500

    add review

--- cars.js
+++ cars.js
@@ -1,28 +1,29 @@
-const cars = [
-    {
-        id: 1,
-        brand: 'Toyota',
-        model: 'Rav-4',
-        color: 'red',
-        year: '2020',
-        price: '500000'
-    },
-    {
-        id: 2,
-        brand: 'KIA',
-        model: 'Seltos',
-        color: 'white',
-        year: '2020',
-        price: '346500'
-    },
-    {
-        id: 3,
-        brand: 'Peigeot',
-        model: '301',
-        color: 'blue',
-        year: '2020',
-        price: '327900'
-    },
-]
-
-export default cars;
+const preLoadedCars = () => {
+    return [
+        {
+            id: 1,
+            brand: 'Toyota',
+            model: 'Rav-4',
+            color: 'red',
+            year: '2020',
+            price: '500000'
+        },
+        {
+            id: 2,
+            brand: 'KIA',
+            model: 'Seltos',
+            color: 'white',
+            year: '2020',
+            price: '346500'
+        },
+        {
+            id: 3,
+            brand: 'Peigeot',
+            model: '301',
+            color: 'blue',
+            year: '2020',
+            price: '327900'
+        },
+    ];
+}
+export default preLoadedCars;
--- crud.js
+++ crud.js
@@ -1,139 +1,94 @@
-import cars from './cars.js';
+import preLoadedCars from './cars.js';
 
+/*Primero acceder a local storage y cargar los datos de local storage si hay, 
+sino escribir los datos desde el arreglo de cars.js
+get item = null, escribir hacia local storage*/
 
-const loadCar = {}
-
-function printCars () {
-    const preList = document.getElementById('pre-list');
-    preList.innerHTML = '';
-
-    cars.forEach( car => {
-        let row = `<tr>
-                        <th>${car.brand}</th>
-                        <th>${car.model}</th>
-                        <th>${car.color}</th>
-                        <th>${car.year}</th>
-                        <th>${car.price}</th>
-                    </tr>`;
-        preList.innerHTML += row;
-    })
+function loadPreList() {
+    //From localStorage
+    const loadCarsFromLocalStorage = () => {
+        return JSON.parse(localStorage.getItem ('carsLabel'));
+    };
+    //From Array preLoadesCars and set at localStorage
+    const loadArrayCarList = () => {
+        if (!localStorage.getItem('carsLabel')) {
+            const arrayCars = preLoadedCars();
+            localStorage.setItem('carsLabel', JSON.stringify(arrayCars, null, 2)
+            );
+        }
+        return loadCarsFromLocalStorage();
+    };
+    // Display for users    
+    const displayCarList = (carsToPrint) => {
+        const carList = document.getElementById('pre-list');
+        const formatterDolar = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        });
+        if (carList) {
+            let rows = '';
+             carsToPrint.forEach( car => {
+                rows += `<tr>
+                            <th>${car.brand}</th>
+                            <th>${car.model}</th>
+                            <th>${car.color}</th>
+                            <th>${car.year}</th>
+                            <th>${formatterDolar.format(car.price)}</th>
+                            <th>
+                            <button onclick="removeCar(${car.id})" class="btn btn-danger">Delete</button>
+                            </th>
+                            <th>
+                            <button onclick="editCar(${car.id})" class="btn btn-warning">Edit</button>
+                            </th>
+                        </tr>`;
+            });
+            carList.innerHTML = rows;
+        }
+    };
+    let cars = loadArrayCarList ();
+    displayCarList(cars);
 }
-printCars();
+loadPreList();
 
-window.onload = function () {
-    
-    var localStorageKeyBrand = 'data';
+document.querySelector('#btn-add').addEventListener('click', function () {
+    var id = getId(),
+        brand = document.getElementById('brand'),
+        model = document.getElementById('model'),
+        color = document.getElementById('color'),
+        year = document.getElementById('year'),
+        price = document.getElementById('price');
 
-    loadFromLocalStorage();
+    // Validate
+    if (brand.value.length === 0 || model.value.length === 0 || color.value.length === 0 || year.value.length === 0 || !parseInt(price.value)) return;
 
-    document.querySelector('#btn-add').addEventListener('click', function () {
-        var brand = document.getElementById('brand'),
-            model = document.getElementById('model'),
-            color = document.getElementById('color'),
-            year = document.getElementById('year'),
-            price = document.getElementById('price');
+    var car = {
+        id: id.value,
+        brand: brand.value,
+        model: model.value,
+        color: color.value,
+        year: year.value,
+        price: price.value
+    };
 
-        // Validate
-        if (brand.value.length === 0 || model.value.length === 0 || color.value.length === 0 || year.value.length === 0 || !parseInt(price.value)) return;
-
-        var car = {
-            brand: brand.value,
-            model: model.value,
-            color: color.value,
-            year: year.value,
-            price: price.value
-        };
-
-        // Clean data
+    // Clean data
+        id.value = '';
         brand.value = '';
         model.value = '';
         color.value = '';
         year.value = '';
         price.value = '';
 
-        // Append to my localStorage
-        appendObjectToLocalStorage(car);
-    })
+    // Append to my localStorage
+    addNewCarToLocalStorage(car);
+});
 
-    function appendObjectToLocalStorage(obj) {
-        var cars = [],
-            dataInLocalStorage = localStorage.getItem(localStorageKeyBrand);
+function addNewCarToLocalStorage(obj) {
+    var localStorageNewCar = 'data';
+    var cars = [],
+        dataInLocalStorage = localStorage.getItem(localStorageNewCar);
 
-        if (dataInLocalStorage !== null) {
-            cars = JSON.parse(dataInLocalStorage);
-        }
-
-        cars.push(obj);
-
-        localStorage.setItem(localStorageKeyBrand, JSON.stringify(cars));
-
-        loadFromLocalStorage();
-    }
-
-    function loadFromLocalStorage() {
-        var cars = [],
-            dataInLocalStorage = localStorage.getItem(localStorageKeyBrand),
-            gridBody = document.querySelector('#grid tbody');
-
-        if (dataInLocalStorage !== null) {
-            cars = JSON.parse(dataInLocalStorage);
-        }
-
-        // Draw TR from TBODY
-        gridBody.innerHTML = '';
-
-        cars.forEach(function (x, i) {
-            var tr = document.createElement('tr'),
-                tdBrand = document.createElement('td'),
-                tdModel = document.createElement('td'),
-                tdColor = document.createElement('td'),
-                tdYear = document.createElement('td'),
-                tdPrice = document.createElement('td'),
-                tdRemove = document.createElement('td'),
-                btnRemove = document.createElement('button');
-            
-            const formatterDolar = new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD'
-            })
-
-            tdBrand.innerHTML = x.brand;
-            tdModel.innerHTML = x.model;
-            tdColor.innerHTML = x.color;
-            tdYear.innerHTML = x.year;
-            tdPrice.innerHTML = formatterDolar.format(x.price);
-
-            
-            // *** Remove
-            btnRemove.textContent = 'Remove';     // Set atributes
-            btnRemove.className = 'btn btn-xs btn-danger';
-            btnRemove.addEventListener('click', function(){
-                removeFromLocalStorage(i);
-            });
-
-            tdRemove.appendChild(btnRemove);
-
-            tr.appendChild(tdBrand);
-            tr.appendChild(tdModel);
-            tr.appendChild(tdColor);
-            tr.appendChild(tdYear);
-            tr.appendChild(tdPrice);
-            tr.appendChild(tdRemove);
-
-            gridBody.appendChild(tr);
-        });
-    }
-
-    function removeFromLocalStorage(index){
-        var cars = [],
-            dataInLocalStorage = localStorage.getItem(localStorageKeyBrand);
-
+    if (dataInLocalStorage !== null) {
         cars = JSON.parse(dataInLocalStorage);
-
-        cars.splice(index, 1);
-
-        localStorage.setItem(localStorageKeyBrand, JSON.stringify(cars));
-
-        loadFromLocalStorage();
     }
+
 }
