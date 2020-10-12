a220f70954bf414203d2b524071f89ed666ca673
Monicavila <di.monicavila@gmail.com>
2020-10-12 01:36:48 -0500

    final version

--- cars.js
+++ cars.js
@@ -2,26 +2,34 @@ const preLoadedCars = [
         {
             id: 1,
             brand: 'Toyota',
-            model: 'Rav-4',
-            color: 'red',
-            year: '2020',
-            price: '500000'
+            model: 'RAV4/AWD XL',
+            color: 'Dark Red Metallic',
+            year: '2021',
+            price: '499100'
         },
         {
             id: 2,
             brand: 'KIA',
             model: 'Seltos',
-            color: 'white',
-            year: '2020',
+            color: 'Snow White Pearl',
+            year: '2021',
             price: '346500'
         },
         {
             id: 3,
             brand: 'Peigeot',
             model: '301',
-            color: 'blue',
-            year: '2020',
+            color: 'Artence Grey',
+            year: '2021',
             price: '327900'
+        },
+        {
+            id: 4,
+            brand: 'Nissan',
+            model: 'Versa',
+            color: 'Sand Stone',
+            year: '2015',
+            price: '275630'
         },
 ];
 export default preLoadedCars;
--- crud.js
+++ crud.js
@@ -2,30 +2,44 @@ import preLoadedCars from './cars.js';
 
 ////VARIABLES
 var editingCar = {}; //Object with edited values
+let cars = loadCarsList(); // Array to apply all functions
 
-/*First access local storage and load local storage data if is available
+////MANAGE LOCAL STORAGE
+/*First access local storage and load local storage data if is available display.
 If not available write the data from the cars.js array (get item = null or ! not exist)
 Last write data in local storage*/
 
 function loadCarsList() {
-    var carList = [],
+    var carsList = [],
         storedList = localStorage.getItem('carsLabel');
-    
     if (storedList !== null) {
-        carList = JSON.parse(storedList);
+        carsList = JSON.parse(storedList);
     }
     else if (!storedList) {
-        carList = preLoadedCars;
+        carsList = preLoadedCars;
     }
-    return carList;
+    return carsList;
 }
 
-function saveLocalStorageCarsList() {
-    const arrayCars = preLoadedCars;
+function saveToLocalStorage() {
+    const arrayCars = cars;
     localStorage.setItem('carsLabel', JSON.stringify(arrayCars));
 }
 
-//Display in screens
+////ASSIGN an ID to a new Car
+function assignId() {
+    let id;
+    let arrayLenght = cars.length;
+    if (!cars.length) {
+        id = 1;
+    }
+    if (cars.length !== 0) {
+        id = cars[arrayLenght-1].id+1;
+    }
+    return id;
+};
+
+////DISPLAY IN SCREENS
 function printCarsToScreen() {   
     const displayCarList = (carsToPrint) => {
         const tBody = document.getElementById('list');
@@ -53,25 +67,11 @@ function printCarsToScreen() {
             tBody.innerHTML = rows;
         }
     };
-    let cars = loadCarsList();
     displayCarList(cars);
 };
 
-////ASSIGN an ID to a new Car
-function assignId() {
-    let id;
-    let arrayLenght = preLoadedCars.length;
-    if (!preLoadedCars.length) {
-        id = 1;
-    }
-    if (preLoadedCars.length !== 0) {
-        id = preLoadedCars[arrayLenght-1].id+1;
-    }
-    return id;
-};
-
 //// ADD A NEW CAR
-document.querySelector('#btn-add').addEventListener('click', function addCarToList() {
+document.querySelector('#btn-add').addEventListener('click', function addACar() {
     const brand = document.getElementById('brand'); //position const in DOM
     const brandValue = brand.value; //save value in new const
     brand.value = ''; //clear data
@@ -89,18 +89,17 @@ document.querySelector('#btn-add').addEventListener('click', function addCarToLi
     price.value = '';
     const id = assignId();
     //Define newCar as an object
-    var carToAdd = {
+    //Add new car to array cars (PreList)
+    cars.push({
         id: id,
         brand: brandValue,
         model: modelValue,
         color: colorValue,
         year: yearValue,
         price: priceValue
-    };
-    //Add new car to array cars (PreList)
-    preLoadedCars.push(carToAdd);
+    });
     //Add new car to locaStorge
-    saveLocalStorageCarsList();
+    saveToLocalStorage();
     //Add new car to inner.HTML
     printCarsToScreen();
 
@@ -112,12 +111,12 @@ function removeCar(id) {
     
     //Pop-op an advice to confirm delete action
     if (window.confirm('Are you sure you want to delete this car?')) { 
-        const index = preLoadedCars.findIndex(car => car.id === id);
+        const index = cars.findIndex(car => car.id === id);
         //Delete car in the array cars (PreList)
-        preLoadedCars.splice(index, 1);
+        cars.splice(index, 1);
     }
     //Delete car in the locaStorge
-    saveLocalStorageCarsList();
+    saveToLocalStorage();
     //Delete car at inner.HTML
     printCarsToScreen();
 };
@@ -125,7 +124,7 @@ function removeCar(id) {
 ////EDIT A CAR
 //Get values as a result of edit a car in the list
 function editCar(idToEdit) {
-    //Active and desactive buttons Save vs Add
+    //Active and desactive buttons Save & Add
     var y = document.getElementById('btn-add');
     if (y.style.display === 'block') {
     y.style.display = 'none';
@@ -135,7 +134,7 @@ function editCar(idToEdit) {
     x.style.display = 'block';
     } 
     //Identify the car is editing
-    editingCar = loadCarsList().find((car) => car.id === idToEdit);
+    editingCar = cars.find((car) => car.id === idToEdit);
     document.getElementById('brand').value = editingCar.brand;
     document.getElementById('model').value = editingCar.model;
     document.getElementById('color').value = editingCar.color;
@@ -144,45 +143,46 @@ function editCar(idToEdit) {
     
 };
 //Save chages in edit car function
-document.querySelector('#btn-save').addEventListener('click', function () {
-    
-    //Active and desactive buttons Add vs Save
-    var y = document.getElementById('btn-add');
-    if (y.style.display === 'none') {
-    y.style.display = 'block';
-    } 
-    var x = document.getElementById('btn-save');
-    if (x.style.display === 'block') {
-    x.style.display = 'none';
+document.querySelector('#btn-save').addEventListener('click', function saveEditedCar() {
+    if (window.confirm('Are you sure you want to save changes?')) {
+        //Active and desactive buttons Add & Save
+        var y = document.getElementById('btn-add');
+        if (y.style.display === 'none') {
+            y.style.display = 'block';
+        } 
+        var x = document.getElementById('btn-save');
+        if (x.style.display === 'block') {
+            x.style.display = 'none';
+        }
+        //Save value per value
+        const editedBrand = document.getElementById('brand'); //Position const in DOM trought the id
+        const editedBrandValue = editedBrand.value; //Get edited values by user
+        editingCar.brand = editedBrandValue; //Save update values and assign to editing car
+        editedBrand.value = ''; //Clear data
+        const editedModel = document.getElementById('model');
+        const editedModelValue = editedModel.value;
+        editingCar.model = editedModelValue;
+        editedModel.value = '';
+        const editedColor = document.getElementById('color');
+        const editedColorValue = editedColor.value;
+        editingCar.color = editedColorValue;
+        editedColor.value = '';
+        const editedYear = document.getElementById('year');
+        const editedYearValue = editedYear.value;
+        editingCar.year = editedYearValue;
+        editedYear.value = '';
+        const editedPrice = document.getElementById('price');
+        const editedPriceValue = editedPrice.value;
+        editingCar.price = editedPriceValue;
+        editedPrice.value = '';
+        
+        //Save update values in the locaStorge
+        saveToLocalStorage();
+        //Save update values at inner.HTML
+        printCarsToScreen();
     }
-    //Save value per value
-    const editedBrand = document.getElementById('brand'); //Position const in DOM trought the id
-    const editedBrandValue = editedBrand.value; //Get edited values by user
-    editingCar.brand = editedBrandValue; //Save update values and assign to editing car
-    editedBrand.value = ''; //Clear data
-    const editedModel = document.getElementById('model');
-    const editedModelValue = editedModel.value;
-    editingCar.model = editedModelValue;
-    editedModel.value = '';
-    const editedColor = document.getElementById('color');
-    const editedColorValue = editedColor.value;
-    editingCar.color = editedColorValue;
-    editedColor.value = '';
-    const editedYear = document.getElementById('year');
-    const editedYearValue = editedYear.value;
-    editingCar.year = editedYearValue;
-    editedYear.value = '';
-    const editedPrice = document.getElementById('price');
-    const editedPriceValue = editedPrice.value;
-    editingCar.price = editedPriceValue;
-    editedPrice.value = '';
-    
-    //Save update values in the locaStorge
-    saveLocalStorageCarsList();
-    //Save update values at inner.HTML
-    printCarsToScreen();
-
 });
+    
 
 printCarsToScreen(); // ALWAYS AFTER ALL THE FUNCTIONS
 window.removeCar = removeCar;
